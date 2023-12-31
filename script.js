c4a9d49e5a12834488b7b0339b06bc96fc0e69d3
Daniel Fahrni <dafahrni@hotmail.com>
2023-12-31 03:02:34 +0100

    play against other user fixes #2

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+class Tile {
+  constructor(element) {
+    this.element = element;
+  }
+
+  get isEmpty() {
+    return this.text === ".";
+  }
+
+  get text() {
+    return this.element.textContent;
+  }
+
+  set text(player) {
+    if (!this.isEmpty) {
+      return;
+    }
+    if (!this.isValid(player)) {
+      return;
+    }
+    this.element.textContent = player;
+    this.element.style.color = "white";
+  }
+
+  reset() {
+    this.element.textContent = ".";
+    this.element.style.color = "transparent";
+  }
+
+  isValid(player) {
+    return player === "X" || player === "O";
+  }
+}
+
 class Game {
   constructor() {
     this.setupGameBoard();
+    this.setupEventListeners();
+    this.reset();
   }
 
   setupGameBoard() {
@@ -11,6 +47,50 @@ class Game {
       const tile = document.createElement("div");
       tile.classList.add("tile");
       gameContainer.appendChild(tile);
+    }
+  }
+
+  setupEventListeners() {
+    const gameContainer = document.querySelector(".game-container");
+    gameContainer.addEventListener("click", this.handleTileClick.bind(this));
+  }
+
+  reset() {
+    this.allTiles().forEach((tile) => tile.reset());
+    this.currentPlayer = "X";
+    console.log(this.asText());
+  }
+
+  allTiles() {
+    return Array.from(
+      document.querySelectorAll(".tile"),
+      (element) => new Tile(element)
+    );
+  }
+
+  asText() {
+    let text = "";
+    let count = 0;
+    for (const tile of this.allTiles()) {
+      count++;
+      text += tile.text;
+      text = count % 3 == 0 ? text + "\n" : text + " ";
+    }
+    return text;
+  }
+
+  handleTileClick(event) {
+    const clickedTile = event.target;
+
+    if (clickedTile.classList.contains("tile")) {
+      const tileObject = new Tile(clickedTile);
+
+      if (tileObject.isEmpty) {
+        tileObject.text = this.currentPlayer;
+        console.log(this.asText());
+
+        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+      }
     }
   }
 }
