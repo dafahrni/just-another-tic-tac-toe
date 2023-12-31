94bc5dc6994ea2d85bcc6811984f58ccff0e516b
Daniel Fahrni <dafahrni@hotmail.com>
2023-12-31 03:15:02 +0100

    determination of winner fixes #3

--- script.js
+++ script.js
@@ -78,6 +78,66 @@ class Game {
     }
     return text;
   }
+  
+  checkForWinner() {
+    // Die Logik für die Überprüfung eines Gewinners
+    // Beispiel: Wenn Sie eine Zeile, Spalte oder Diagonale mit dem gleichen Spieler finden, gibt es einen Gewinner.
+    const rows = this.asText().split("\n");
+    const board = Array.from(rows, (row) => row.split(" "));
+
+    // Überprüfen von horizontalen Linien
+    for (let row = 0; row < 3; row++) {
+      if (
+        board[row][0] !== "." &&
+        board[row][0] === board[row][1] &&
+        board[row][1] === board[row][2]
+      ) {
+        return true; // Gewonnen
+      }
+    }
+
+    // Überprüfen von vertikalen Linien
+    for (let col = 0; col < 3; col++) {
+      if (
+        board[0][col] !== "." &&
+        board[0][col] === board[1][col] &&
+        board[1][col] === board[2][col]
+      ) {
+        return true; // Gewonnen
+      }
+    }
+
+    // Überprüfen der Diagonalen
+    if (
+      board[0][0] !== "." &&
+      board[0][0] === board[1][1] &&
+      board[1][1] === board[2][2]
+    ) {
+      return true; // Gewonnen
+    }
+
+    if (
+      board[0][2] !== "." &&
+      board[0][2] === board[1][1] &&
+      board[1][1] === board[2][0]
+    ) {
+      return true; // Gewonnen
+    }
+
+    return false; // Kein Gewinner
+  }
+
+  checkForDraw() {
+    // Überprüfen, ob alle Kacheln belegt sind
+    for (const tile of this.allTiles()) {
+      if (tile.isEmpty) {
+        // Es gibt mindestens eine leere Kachel, also kein Unentschieden
+        return false;
+      }
+    }
+    // Alle Kacheln sind belegt, es ist ein Unentschieden
+    return true;
+  }
 
   handleTileClick(event) {
     const clickedTile = event.target;
@@ -89,10 +149,51 @@ class Game {
         tileObject.text = this.currentPlayer;
         console.log(this.asText());
 
-        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+        if (this.checkForWinner()) {
+          showAlert(`Spieler ${this.currentPlayer} gewinnt!`);
+        } else if (this.checkForDraw()) {
+          showAlert("Unentschieden!");
+        } else {
+          this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+        }
       }
     }
   }
 }
 
 const game = new Game();
+
+let notificationTimeout;
+
+function showAlert(message) {
+  // Modal-Box mit Nachricht wird angezeigt
+  const notificationModal = document.getElementById("notificationModal");
+  const overlay = document.getElementById("overlay");
+  const messageElement = document.getElementById("notificationMessage");
+
+  // Setzen der Nachricht in die Modal-Box
+  messageElement.textContent = message;
+
+  // Anzeigen der Modal-Box
+  notificationModal.style.display = "block";
+  overlay.style.display = "block";
+
+  // Neues Timeout starten
+  notificationTimeout = setTimeout(function () {
+    hideNotification();
+  }, 5000);
+}
+
+function hideNotification() {
+  // Modal-Box wird ausgeblendet
+  const notificationModal = document.getElementById("notificationModal");
+  const overlay = document.getElementById("overlay");
+
+  notificationModal.style.display = "none";
+  overlay.style.display = "none";
+
+  game.reset();
+
+  // Timeout zurücksetzen
+  clearTimeout(notificationTimeout);
+}
